92f0953bc50d40b93180bf370b598ff6e982edb7
Alvin Coulson <aaacoulson@gmail.com>
2020-06-20 22:20:38 -0400

    migrate 'create_dealers' and seed with data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Dealer.create(name: "Railsland Jaguar")
+puts "Dealer successfully created" if Dealer.first
