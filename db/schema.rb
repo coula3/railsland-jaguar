2eceb625d2954f57ddea4115d0c6a5e9fb16e374
Alvin Coulson <aaacoulson@gmail.com>
2020-06-20 23:11:16 -0400

    migrate 'create_services' and seed with data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_024129) do
+ActiveRecord::Schema.define(version: 2020_06_21_025523) do
 
   create_table "dealers", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2020_06_21_024129) do
     t.string "telephone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "name"
+    t.integer "dealer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["dealer_id"], name: "index_services_on_dealer_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,5 +42,6 @@ ActiveRecord::Schema.define(version: 2020_06_21_024129) do
     t.index ["dealer_id"], name: "index_users_on_dealer_id"
   end
 
+  add_foreign_key "services", "dealers"
   add_foreign_key "users", "dealers"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# DEALER ***
 # Dealer.create(name: "Railsland Jaguar")
 # puts "Dealer successfully created" if Dealer.first
 
-User.create(first_name: "Josh", last_name: "Powers", email: "jpowers@rjaguar.com", dealer_id: Dealer.first.id)
-puts "User successfully created" if User.first
+# USER ***
+# User.create(first_name: "Josh", last_name: "Powers", email: "jpowers@rjaguar.com", dealer_id: Dealer.first.id)
+# puts "User successfully created" if User.first
+
+# SERVICE ***
+services = ["routine maintenance", "inspection", "electronics-system", "air-conditioning"]
+
+services.each do |service|
+    Service.create(name: service, dealer_id: Dealer.first.id)
+end
+puts "#{Service.count} services successfully created!" if Service.count == services.count
