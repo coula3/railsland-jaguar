fd544fda2551ee4d8d5ab3ff91019f9bc53e22dc
Alvin Coulson <aaacoulson@gmail.com>
2020-06-21 21:05:34 -0400

    create user and add user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    # raise params.inspect
+    @user = Dealer.first.users.build(user_params)
+    if @user.save
+      session[:id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   def index
@@ -15,5 +24,10 @@ class UsersController < ApplicationController
   end
 
   def update
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 end
