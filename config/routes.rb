b5edd4022e80870029d2664594960b0b4215a080
Alvin Coulson <aaacoulson@gmail.com>
2020-06-22 12:37:39 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  root 'application#home'
-  get '/signup', to: 'users#signup'
-  get '/signin', to: 'session#signin'
-  post '/session', to: 'session#create'
+  root 'session#new'
+  post '/signin', to: 'session#create'
+  get '/signup', to: 'users#new'
   get '/signout', to: 'session#destroy'
   get '/users/workspace', to: 'users#workspace', as: 'user_workspace'
 
