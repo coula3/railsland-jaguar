782bc3e47798b76c7b233b608195becd9c28cb2d
Alvin Coulson <aaacoulson@gmail.com>
2020-06-22 14:01:44 -0400

    set up user update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,12 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find_by(id: params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   def workspace
