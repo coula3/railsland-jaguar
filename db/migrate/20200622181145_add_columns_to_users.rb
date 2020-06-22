008de05576695efa8d543dad347897e00fd4f9a5
Alvin Coulson <aaacoulson@gmail.com>
2020-06-22 15:42:26 -0400

    add new columns to users

---
+++ db/migrate/20200622181145_add_columns_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnsToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :admin, :boolean, default: false
+    add_column :users, :status, :string, default: :active
+    add_column :users, :position, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_040309) do
+ActiveRecord::Schema.define(version: 2020_06_22_181145) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "date"
@@ -64,6 +64,9 @@ ActiveRecord::Schema.define(version: 2020_06_21_040309) do
     t.integer "dealer_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
+    t.string "status", default: "active"
+    t.string "position"
     t.index ["dealer_id"], name: "index_users_on_dealer_id"
   end
 
