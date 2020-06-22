1371f1df211fce70597bebf7d2aee21b33778d35
Alvin Coulson <aaacoulson@gmail.com>
2020-06-22 12:45:51 -0400

    rename action - from signup to new

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,5 +1,5 @@
 class SessionController < ApplicationController
-  def signin
+  def new
   end
 
   def create
@@ -8,12 +8,12 @@ class SessionController < ApplicationController
       session[:user_id] = user.id
       redirect_to user_workspace_path
     else
-      render :signin
+      render :new
     end
   end
 
   def destroy
     session.delete(:user_id)
-    redirect_to signin_path
+    redirect_to root_path
   end
 end
