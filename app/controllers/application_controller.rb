8edc2976998619044b5a2cf8869bc53d9b0b7a7f
Alvin Coulson <aaacoulson@gmail.com>
2020-06-22 01:56:34 -0400

    rename key in session hash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
     def current_user
-        @user ||= User.find_by(id: session[:id]) if session[:id]
+        @user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 end
