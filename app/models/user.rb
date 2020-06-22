17b4e702c8f8e7253111455bd850993714a54497
Alvin Coulson <aaacoulson@gmail.com>
2020-06-22 15:48:32 -0400

    add admin? and sys_role methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
   def full_name
     self.first_name + " " + self.last_name
   end
+  
+  def admin?
+    self.admin ? "Yes" : "No"
+  end
+
+  def sys_role
+    self.admin ? "Admin" : "User"
+  end
+
 end
