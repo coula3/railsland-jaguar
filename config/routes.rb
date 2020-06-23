d4c33eab7dd10a554f934a616d8da73749d5100c
Alvin Coulson <aaacoulson@gmail.com>
2020-06-23 01:38:45 -0400

    draw route for callback

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   get '/signout', to: 'session#destroy'
   get '/users/workspace', to: 'users#workspace', as: 'user_workspace'
+  get '/auth/facebook/callback', to: 'session#create'
 
   resources :users, only: [:create, :show, :index, :edit, :update, :destroy]
 end
