c45319013b301e874c37bf9155bfc78ab32353d3
Alvin Coulson <aaacoulson@gmail.com>
2020-06-23 16:59:04 -0400

    set up service new and create actions and strong params

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,8 +1,15 @@
 class ServicesController < ApplicationController
   def new
+    @service = Service.new
   end
 
   def create
+    @service = Dealer.first.services.create(service_params)
+    if @service
+      redirect_to services_path
+    else
+      render :new
+    end
   end
 
   def index
@@ -18,5 +25,11 @@ class ServicesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def service_params
+    params.require(:service).permit(:name)
   end
 end
