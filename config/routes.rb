d72d2b3c5e884e20989baa75931bc116f07696fe
Alvin Coulson <aaacoulson@gmail.com>
2020-06-23 15:32:24 -0400

    draw services routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  # get 'customers/new'
-  # get 'customers/create'
-  # get 'customers/index'
-  # get 'customers/show'
-  # get 'customers/edit'
-  # get 'customers/update'
-  # get 'customers/destroy'
   root 'session#new'
   post '/signin', to: 'session#create'
   get '/signup', to: 'users#new'
@@ -16,4 +9,5 @@ Rails.application.routes.draw do
 
   resources :users, only: [:create, :show, :index, :edit, :update, :destroy]
   resources :customers
+  resources :services
 end
