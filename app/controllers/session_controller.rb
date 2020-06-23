f700c6d650b8b31e1a6f3613737fbbb9fa999ee3
Alvin Coulson <aaacoulson@gmail.com>
2020-06-23 01:43:14 -0400

    update create action for omniauth

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,17 +3,36 @@ class SessionController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to user_workspace_path
+    if auth
+      @user = User.find_or_create_by(uid: auth['uid']) do |u|
+        u.email = auth['info']['email']
+        u.image = auth['info']['image']
+        u.password = SecureRandom.hex
+        u.dealer_id = Dealer.first.id
+      end
+
+      session[:user_id] = @user.id
+      render 'users/workspace'
+
     else
-      render :new
+      user = User.find_by(email: params[:email])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to user_workspace_path
+      else
+        render :new
+      end
     end
   end
-
+  
   def destroy
     session.delete(:user_id)
     redirect_to root_path
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
