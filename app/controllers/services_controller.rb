b15aec86aa7dc0d4901414e8c7cb3c22ae8cb577
Alvin Coulson <aaacoulson@gmail.com>
2020-06-23 17:58:38 -0400

    set up service edit and update actions and build service edit view

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -20,9 +20,16 @@ class ServicesController < ApplicationController
   end
 
   def edit
+    @service = Service.find(params[:id])
   end
 
   def update
+    @service = Service.find(params[:id])
+    if @service.update(service_params)
+      redirect_to services_path
+    else
+      render :edit
+    end
   end
 
   def destroy
