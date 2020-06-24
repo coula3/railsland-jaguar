7a50063ec362dfba5231e522c5157fbd3c0a78fe
Alvin Coulson <aaacoulson@gmail.com>
2020-06-23 22:26:56 -0400

    refactor fullname to fix associated nilclass error

--- app/controllers/concerns/full_name.rb
+++ app/controllers/concerns/full_name.rb
@@ -1,7 +1,7 @@
 module FullName
     module InstanceMethods
         def full_name
-            self.first_name + " " + self.last_name
+            ["#{self.first_name}", "#{self.last_name}"].join(" ")
         end
     end
 end
