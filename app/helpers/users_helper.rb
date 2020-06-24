e3d9d4d446c8ae00ae6ba9b87679f48984d759af
Alvin Coulson <aaacoulson@gmail.com>
2020-06-23 22:29:07 -0400

    add user_image and user_first_name helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,18 @@ module UsersHelper
     def user_status
        status = ["active", "inactive", "deactivated"]
     end
+
+    def user_image
+        if current_user.image
+            img ||= current_user.image
+        end
+    end
+
+    def user_first_name
+        if current_user.first_name
+            current_user.first_name.capitalize + "'s"
+        else
+            "My"
+        end
+    end
 end
