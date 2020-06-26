8417a922964693a614f9792bac46f8536084f416
Alvin Coulson <aaacoulson@gmail.com>
2020-06-25 21:51:48 -0400

    helper for methods - app_status, veh_models, model_years

--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -1,2 +1,14 @@
 module AppointmentsHelper
+    def app_status
+        status ||= ["open", "rescheduled", "cancelled", "no-show", "service completed"]
+    end
+
+    def veh_models
+        models ||= ["Jaguar F-TYPE", "Jaguar XE", "Jaguar XE SV", "Jaguar XF", "Jaguar XJ", "Jaguar E-PACE", "Jaguar F-PACE", "Jaguar I-PACE"]
+    end
+
+    def model_years
+        t = Time.now
+        m_years ||=  ((t.year - 10)..t.year).to_a
+    end
 end
