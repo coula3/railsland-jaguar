c2fd175e0c2e61e09c31ddba1fcf4cc608717e9b
Alvin Coulson <aaacoulson@gmail.com>
2020-06-25 21:48:44 -0400

    helper to build collection radio buttons for services

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -1,2 +1,10 @@
 module ServicesHelper
+    def service_select(app, path)
+        if !app.service
+            hidden_field_tag "app[service_id]", app.service_id
+            collection_radio_buttons(:service, :service_id, Service.all, :id, :name) do |b|
+                b.label { b.radio_button + " " + b.text }
+            end
+        end
+      end
 end
