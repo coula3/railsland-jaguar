c8ee94a43e630265bb22e45e8fb942f8848df5a4
Alvin Coulson <aaacoulson@gmail.com>
2020-06-28 11:44:27 -0400

    build dealers edit and update actions as well as dealer edit view

--- app/controllers/dealers_controller.rb
+++ app/controllers/dealers_controller.rb
@@ -1,7 +1,24 @@
 class DealersController < ApplicationController
+  before_action :set_dealer
+
   def edit
   end
 
   def update
+    if @dealer.update(dealer_params)
+      redirect_to user_workspace_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def dealer_params
+    params.require(:dealer).permit(:name, :address, :city, :state, :zip, :telephone)
+  end
+
+  def set_dealer
+    @dealer = Dealer.find_by(id: params[:id])
   end
 end
