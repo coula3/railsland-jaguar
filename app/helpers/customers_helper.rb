1f03f72176aa3b2b72cb98907eedcf3113d63abd
Alvin Coulson <aaacoulson@gmail.com>
2020-06-30 15:42:09 -0400

    define new_appointment(customer) helper to replace link_to helper in various views

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -10,4 +10,8 @@ module CustomersHelper
     def pluralize_customer(collection)
         pluralize(collection.size, 'Customer').dup.concat(" with Service Insurance")
     end
+
+    def new_appointment(customer)
+        link_to 'Create New Appointment', new_customer_appointment_path(customer)
+    end
 end
