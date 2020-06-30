f2853aac2de46c3592ba782944315e2e3d6b2fa0
Alvin Coulson <aaacoulson@gmail.com>
2020-06-30 09:17:04 -0400

    add 'service_customers' helper to replace link_to helpers used in various views

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -2,4 +2,8 @@ module CustomersHelper
     def select_service_insurance
         options ||= {"Yes" => true, "No" => false} 
     end
+
+    def service_customers
+        link_to 'Service Customers', customers_path
+    end
 end
