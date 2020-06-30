7dfb4d46ee4d803db79ca3e76e2fc9b017449355
Alvin Coulson <aaacoulson@gmail.com>
2020-06-30 11:59:41 -0400

    add customers helper to pluralize count of customer in header on view for customer with service insurance

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -6,4 +6,8 @@ module CustomersHelper
     def service_customers
         link_to 'Service Customers', customers_path
     end
+
+    def count_of_customers_with_service_insurance(collection)
+        pluralize(collection.size, 'Customer').dup.concat(" with Service Insurance")
+    end
 end
