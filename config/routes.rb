1aa51865c7df2932a4dbdcbc7f302ddebaf6ca02
Alvin Coulson <aaacoulson@gmail.com>
2020-07-01 14:00:28 -0400

    add POST /signup route for render action when user create action is unsuccessful

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'sessions#new'
   post '/signin', to: 'sessions#create'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
   get '/signout', to: 'sessions#destroy'
   get '/users/workspace', to: 'users#workspace', as: 'user_workspace'
   get '/auth/facebook/callback', to: 'sessions#create'
