8d4049b2f622e8ddec48edc627fddcd814719852
Alvin Coulson <aaacoulson@gmail.com>
2020-06-30 21:58:25 -0400

    rename controller from session to sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-class SessionController < ApplicationController
+class SessionsController < ApplicationController
   before_action :require_login
   skip_before_action :require_login, only: [:new, :create]
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root 'session#new'
-  post '/signin', to: 'session#create'
+  root 'sessions#new'
+  post '/signin', to: 'sessions#create'
   get '/signup', to: 'users#new'
-  get '/signout', to: 'session#destroy'
+  get '/signout', to: 'sessions#destroy'
   get '/users/workspace', to: 'users#workspace', as: 'user_workspace'
-  get '/auth/facebook/callback', to: 'session#create'
+  get '/auth/facebook/callback', to: 'sessions#create'
   get '/users/:id/admin_edit', to: 'users#admin_edit', as: 'admin_edit'
   get '/customers/with_service_insurance', to: 'customers#with_service_insurance', as: 'service_insurance'
 
