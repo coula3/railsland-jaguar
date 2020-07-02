c36c5d0ef0c311333d68ad1224f0847dcd48d749
Alvin Coulson <aaacoulson@gmail.com>
2020-07-01 22:43:42 -0400

    define 'access_unauthorized_msg' helper method for multiple use message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_user, :logged_in?
+    helper_method :current_user, :logged_in?, :access_unauthorized_msg
 
     def logged_in?
         !!current_user
@@ -7,5 +7,9 @@ class ApplicationController < ActionController::Base
 
     def current_user
         user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+    def access_unauthorized_msg
+        "Access Unauthorized"
     end
 end
