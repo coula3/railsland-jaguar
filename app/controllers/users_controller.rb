b39c674f847c8b590814e743a9366b6e312b8ca6
Alvin Coulson <aaacoulson@gmail.com>
2020-07-27 15:23:34 -0400

    sanitise password_confirmation on UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -83,7 +83,7 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :sys_role, :email, :password, :status, :admin)
+    params.require(:user).permit(:first_name, :last_name, :sys_role, :email, :password, :password_confirmation, :status, :admin)
   end
 
   def require_login
