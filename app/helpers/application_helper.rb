79f6314e36308315b0d9c80a47dcbebd5cc7385f
Alvin Coulson <aaacoulson@gmail.com>
2021-01-10 19:57:08 -0500

    rename helper method from 'last_first_name' to 'last_name_first_name'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,10 @@ module ApplicationHelper
         ["#{user.first_name}", "#{user.last_name}"].join(" ")
     end
 
+    def last_name_first_name(user)
+        ["#{user.last_name.upcase},", "#{user.first_name}"].join(" ")
+    end
+
     def readable_created_at(object)
         object.created_at.strftime("%b %d, %Y")
     end
