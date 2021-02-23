e13909e728647b24e1d01ae6527b480dd35cc044
Alvin Coulson <aaacoulson@gmail.com>
2021-02-22 20:09:27 -0500

    restore column heading of 'Insurance' on appointments index

--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -25,7 +25,7 @@ module AppointmentsHelper
     end
 
     def display_service_insurance_header(collection)
-        "Ins" if collection.distinct.count(:customer_id) > 1
+        "Insurance" if collection.distinct.count(:customer_id) > 1
     end
 
     def service_appointments
